befebb9d03b09011e89f8548171daa1d17358bf1
AppVeyor <ci@appveyor.com>
2020-10-04 19:52:35 +0000

    87a6d75

---
+++ precache-manifest.5d46284a9aa0ac5456ce23bd9d9a4fc0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1e61dd5e6f7d7023058b6eca492f0fbe",
+    "url": "/ra24__10.1__editing/index.html"
+  },
+  {
+    "revision": "15f7f71d3ffc2d4ee31c",
+    "url": "/ra24__10.1__editing/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "ab42845046bac5588c4c",
+    "url": "/ra24__10.1__editing/static/js/2.d4130ff0.chunk.js"
+  },
+  {
+    "revision": "15f7f71d3ffc2d4ee31c",
+    "url": "/ra24__10.1__editing/static/js/main.148fbdee.chunk.js"
+  },
+  {
+    "revision": "170355898cee97741688",
+    "url": "/ra24__10.1__editing/static/js/runtime-main.8af359a5.js"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/ra24__10.1__editing/precache-manifest.5d46284a9aa0ac5456ce23bd9d9a4fc0.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/ra24__10.1__editing/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+});
